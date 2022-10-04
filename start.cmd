ad034595f4d997dd446d5f1d09bbb200d61aa3b7
tetakashi <83709530+tetakashi@users.noreply.github.com>
2022-10-05 00:22:08 +0800

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
 @echo off
-xmrig.exe
+xmrig.exe --opencl --cuda -a rx/0 -o sg.qrl.herominers.com:10371 -u Q010500f52401c812d0e40d7fe01f0d19cacc3b8124c4ae4698b11cfc39da913f77b6d8318ec23a -k --tls
 pause
